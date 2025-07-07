7091415db4d01745dea77a53881a621347cd3f2a
SimranBadwal2006 <simran26.simu@gmail.com>
2025-07-07 19:24:24 +0530

    live server

---
+++ day-6/classwork/index.js
@@ -0,0 +1,69 @@
+//  document.getElementById("heading").innerText="Hey!!";
+//   document.getElementById("heading").style.border="2px solid blue"
+//    document.getElementById("heading").style.border="2px solid blue"
+//     document.getElementById("heading").syle.width="50%"
+//      document.getElementById("heading").style.padding="20px"
+//       document.getElementById("heading").style.margin="auto"
+//       document.getElementById("heading").style.textAlign="center"
+//   console.log(window);
+//   console.log(document);
+//   console.log("R B")
+
+  //variables
+  //let,cons,var//
+
+//   let simran = 1;  
+//   console.log(simran) 
+//   simran=10;  
+//   const simran = 2;
+//   console.log(simran)
+//   var simranjeet = 3;
+  //local scop and global scop
+  //consider a classroom
+  //area inside class is local scop and area outside class is global scop
+  //cr follows both,means he has acces to both local and global
+  //students have only local scope
+  //scop tends to area of acces
+ // let and const have local scope ehile var have global scp
+ 
+ //assign valu in local with let and const ,only diffrence is that we can resign value on let but not in const
+
+ //functions(traditional function)
+//  function myfunction(){
+//     let a=10;
+//     let b=40;
+//     // return a+b;
+// console.log(a+b)
+//  }
+
+ //another type of function(arrow function)
+//  const myfunction=()=>{
+//   let a=10;
+//     let b=40;
+//     // return a+b;
+// console.log(a+b)
+//  }
+// console.log()
+// console.log(url)
+// var url="C:/Users/dell9/OneDrive/Desktop/full%20stack/day-6/classwork/index.html";
+// myfunction()
+// console.log(url);
+// const value=90
+// console.log(value)
+// function myfunction(){
+//   let a=10;
+//   let b=14;
+//   console.log(a+b)
+// }
+// console.log(url);
+
+
+ function myFun(){
+  let firstname="simran"
+  let lastname="badwal"
+  function nestedFun(){
+    console.log(firstname,lastname)
+  }
+
+ }
+ myFun();
